b082dd223f1e13226ebe6a07d063f24227c1e761
Caio Simplicio <caio1simplicio@gmail.com>
2024-03-22 17:46:29 -0300

    upgrading coverage

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,23 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    it("Deve renderizar a inserção de dois comentários;", () => {
+        const {debug} = render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('test-textarea'), {
+            target: {
+                value: 'Comentário 1'
+            }
+        })
+        fireEvent.click(screen.getByTestId('test-button'))
+
+        fireEvent.change(screen.getByTestId('test-textarea'), {
+            target: {
+                value: 'Comentário 2'
+            }
+        })
+        fireEvent.click(screen.getByTestId('test-button'))
+
+        debug()
+        expect(screen.getAllByTestId('test-comment')).toHaveLength(2);
+    })
 });
